e584d9de91b4edd7a8a7f55ca70990e90002f38a
Piotr Domian <domianpiotr111@gmail.com>
2021-04-20 21:29:18 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function selectItem() {
   let select = document.createElement("select");
   select.setAttribute("id", "level");
-  for (var i = 0; i < 3; i++)
-   {
-     let option = document.createElement("option");
-     option.setAttribute("value", 1,2,3);
-     select.appendChild(option);
-   }
+  // for (var i = 0; i < 3; i++)
+  //  {
+  //    let option = document.createElement("option");
+  //    option.setAttribute("value", i+1);
+  //    select.appendChild(option);
+  //  }
+  [1,2,3].forEach(n => {
+    let option = document.createElement("option");
+    option.setAttribute("value", n);
+    select.appendChild(option);
+  })
   document.body.appendChild(select);
   select.addEventListener('change', (event) => {
-    window.alert();
+    window.alert(event.target.value);
   });
   return select;
 };
 
 module.exports = selectItem;
+
+
+/**Stwórz funkcję selectItem tworzącą element <select> (użyj document.createElement), o identyfikatorze “level”, który będzie posiadał trzy elementy <option>
+ *  z atrybutami value z cyframi od 1 do 3 i dodaj go do elementu <body>. Funkcja powinna także zwracać element <select>. Dodaj w środku tej funkcji obsługę zdarzenia change elementu <select>,
+ *  która po zajściu zdarzenia wywoła funkcję window.alert z wybraną opcją z elementu <select>.  */
