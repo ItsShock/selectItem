9426ab52c6d632e19d0996568c165b19174c5722
Piotr Domian <domianpiotr111@gmail.com>
2021-04-20 20:30:55 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function selectItem() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+  let select = document.createElement("select");
+  select.setAttribute("id", "level");
+  for (var i = 0; i < 3; i++)
+   {
+     let option = document.createElement("option");
+     option.setAttribute("value", 1,2,3);
+     select.appendChild(option);
+   }
+  document.body.appendChild(select);
+  select.addEventListener('change', (event) => {
+    window.alert();
+  });
+  return select;
 };
 
 module.exports = selectItem;
